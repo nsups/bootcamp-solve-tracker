45035427050cac781e4c45dc60e5031e08c18321
withtahmid <tahmid.rakib@northsouth.edu>
2024-09-06 01:16:11 +0600

    first commit

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,8 +1,7 @@
 import { Season } from "../types/Season"
 import s18  from "./seasons/s18";
-import { Contest } from "../types/Season";
 
 const seasons: Record<string, Season> = {
-    s18: s18,
+    s18,
 }
 export default seasons;
